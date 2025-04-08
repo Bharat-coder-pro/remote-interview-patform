c97ff92db2e0e7960238d1ed1e8919b7c8ae4e33
Bharat Yadav <111577717+Bharat-coder-pro@users.noreply.github.com>
2025-04-08 12:39:29 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,9 +13,8 @@ function Navbar() {
           href="/"
           className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
         >
-          <CodeIcon className="size-8 text-emerald-500" />
           <span className="bg-gradient-to-r from-emerald-600 font-bold to-teal-500 bg-clip-text text-transparent">
-            HirePro
+            {`<HirePro />`}
           </span>
         </Link>
 
